7519dd38e57675f9029232db72c024357e8ae844
Federico Depetris <depetrisfe@gmail.com>
2021-06-07 13:48:32 -0300

    primera version intentando testear subiendo a git

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="padre">
+      <div className="main">
+        <header>
+          <p>useState Hook First Test</p>
+        </header>
+        <div className="display">
+          <p>{count}</p>
+        </div>
+        <div>
+          <button className="btn-count" onClick={() => setCount(count + 1)}>
+            A
+          </button>
+          <button
+            className="btn-count"
+            onClick={() => setCount(count == 0 ? 0 : count - 1)}
+          >
+            B
+          </button>
+          <button className="btn-reset" onClick={() => setCount(0)}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
