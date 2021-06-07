79e22be29ae906de4d2023bfb80b7f999e6ac2ce
Federico Depetris <depetrisfe@gmail.com>
2021-06-07 14:02:04 -0300

    Tercera prueba subiendo a GIT

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="padre">
       <div className="main">
         <header>
-          <p>useState Hook Test</p>
+          <p>useState Hook Test for GIT</p>
         </header>
         <div className="display">
           <p>{count}</p>
